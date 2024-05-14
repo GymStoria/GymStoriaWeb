58ae382a02857dc22b9320b086b683ca5e2cb206
labib21sa1193 <labibnurali18@gmail.com>
2024-05-14 13:02:39 +0700

    test

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,15 @@
+import { Container } from "react-bootstrap"
+
 const LandingPage = () => {
   return (
     <div className="landingpage">
-      <header className="w-100 min-vh-100"></header>
+      <header className="w-100 min-vh-100">
+        <Container>
+          
+
+          <h1>hallo</h1>
+        </Container>
+      </header>
     </div>
   )
 }
