05b8f3686d4c1b1d483a3f7da868f1ae9a518e15
labib21sa1193 <labibnurali18@gmail.com>
2024-05-22 00:39:39 +0700

    Update

--- src/pages/DetailProdukPage.jsx
+++ src/pages/DetailProdukPage.jsx
@@ -1,19 +1,51 @@
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col } from "react-bootstrap";
+
+import fotoGym from "../assets/img/gym/gym1.jpg";
+import iconPeta from "../assets/img/icon/peta.svg";
+import iconKalender from "../assets/img/icon/kalender.svg";
+import iconToilet from "../assets/img/icon/toilet.svg";
+import iconjam from "../assets/img/icon/jam.svg";
 
 const DetailProdukPage = () => {
   return (
-    <div>
-      <Container>
-        <Row>
-          <Col>
-            <div>
-              <h1>Detail Produk</h1>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+    <div className="detailProdukPage min-vh-100">
+      <header className="header">
+        <Container>
+          <Row>
+            <Col>
+              <h1>INFORMASI DETAIL</h1>
+            </Col>
+          </Row>
+        </Container>
+      </header>
+      <div className="galery">
+        <Container>
+          <Row>
+            <Col>
+              <img src={fotoGym} alt="" />
+              <h1>The Jade</h1>
+            </Col>
+            <Col>
+              <img src={fotoGym} alt="" />
+              <img src={fotoGym} alt="" />
+            </Col>
+          </Row>
+    
+          <Row>
+            <Col>
+              <div className="icon">
+                <img src={iconPeta} alt="" />
+                <h3>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Magni, ut.
+                </h3>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DetailProdukPage
+export default DetailProdukPage;
